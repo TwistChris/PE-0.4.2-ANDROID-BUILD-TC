a119aa3a05e33324e0b7339ac47959e81d894934
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-03-27 15:38:23 +0200

    Update script.lua

--- assets/preload/data/final-destination/script.lua
+++ assets/preload/data/final-destination/script.lua
@@ -71,8 +71,7 @@ function onUpdate()
 	    if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
                 triggerEvent('Camera Follow Pos',xx2,yy2)
             end
-         end
-     else
+        else
 
             if getProperty('dad2.animation.curAnim.name') == 'singLEFT' then
                 triggerEvent('Camera Follow Pos',xx3-ofs,yy3)
