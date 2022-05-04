6a5a35b6af5aac018ccad0a82a0f318d2a13bb82
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-05-04 22:02:37 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -963,7 +963,8 @@ class PlayState extends MusicBeatState
                         animatediconP1.animation.addByPrefix('losing', 'a2', 24, true);
                         animatediconP1.animation.play('normal');
                         animatediconP1.cameras = [camHUD];
-                        animatediconP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 0, 100, 100, 0) * 0.01) - animatediconP1.width);
+                        animatediconP1.x = iconP1.x;
+                        animatediconP1.angle = iconP1.angle;
 		        animatediconP1.y = iconP1.y + -70;
                         animatediconP1.flipX = true;
                         add(animatediconP1);
@@ -988,6 +989,7 @@ class PlayState extends MusicBeatState
                         animatediconP2.animation.addByPrefix('losing', 'a2', 24, true);
                         animatediconP2.animation.play('normal');
                         animatediconP2.cameras = [camHUD];
+                        animatediconP2.x = iconP2.x;
                         animatediconP2.angle = iconP2.angle;
                         animatediconP2.y = iconP2.y + -70;
                         add(animatediconP2);
