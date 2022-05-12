370f95f84b02a66e8cb386c6298eac93864ed155
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-05-12 19:54:53 +0200

    Update PlayStateFr.hx

--- source/PlayStateFr.hx
+++ source/PlayStateFr.hx
@@ -63,22 +63,22 @@ import ui.Mobilecontrols;
 
 using StringTools;
 
-class PlayState extends MusicBeatState
+class PlayStateFr extends MusicBeatState
 {
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
+		['Tu crains!', 0.2], //From 0% to 19%
+		['Crotte', 0.4], //From 20% to 39%
+		['Mauvais', 0.5], //From 40% to 49%
 		['Bruh', 0.6], //From 50% to 59%
 		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Joli', 0.7], //69%
+		['Bon', 0.8], //From 70% to 79%
+		['Génial', 0.9], //From 80% to 89%
+		['Malade!', 1], //From 90% to 99%
+		['Parfait!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	#if (haxe >= "4.0.0")
