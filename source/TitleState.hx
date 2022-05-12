c933665bf3d929d7ec3c002ed395ceaac39d2412
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-05-12 18:17:49 +0200

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -130,7 +130,7 @@ class TitleState extends MusicBeatState
 		#elseif CHARTING
 		MusicBeatState.switchState(new ChartingState());
 		#else
-		if(FlxG.save.data.language == null && !TranslateState.leftState) {
+		if(FlxG.save.data.languageselect == null && !TranslateState.leftState) {
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
 			MusicBeatState.switchState(new TranslateState());
