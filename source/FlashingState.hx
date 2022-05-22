b44ea392cd11fbad19f876ad267db4084735028d
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-05-22 12:11:22 +0200

    Update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -66,7 +66,7 @@ class FlashingState extends MusicBeatState
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
 						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleState());
+							MusicBeatState.switchState(new TitleStateFirst());
 						}
 					});
 				}
