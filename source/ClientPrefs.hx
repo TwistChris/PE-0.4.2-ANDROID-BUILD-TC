d47984e8fb42266d667b8fbbb7695cd4e171f0cd
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-07-08 16:07:56 +0200

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -7,11 +7,11 @@ import flixel.graphics.FlxGraphic;
 import Controls;
 
 class ClientPrefs {
+        public static var language:String = 'en';
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
 	public static var showFPS:Bool = true;
 	public static var flashing:Bool = true;
-        public static var language:Bool = true;
 	public static var globalAntialiasing:Bool = true;
 	public static var noteSplashes:Bool = true;
 	public static var lowQuality:Bool = false;
