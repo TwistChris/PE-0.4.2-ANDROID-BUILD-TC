3019cef06d5a64e4b370cefcb114514345d7d20f
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-07-09 20:46:49 +0200

    Update FlashingState.hx

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -34,10 +34,10 @@ class FlashingState extends MusicBeatState
 		warnText.screenCenter(Y);
 		add(warnText);
 
-                if(FlxG.save.data.language == null && !TranslateStateFirst.leftState) {
+                if(FlxG.save.data.language == null && !TranslateState.leftState) {
 			FlxTransitionableState.skipNextTransIn = true;
 			FlxTransitionableState.skipNextTransOut = true;
-			MusicBeatState.switchState(new TranslateStateFirst());
+			MusicBeatState.switchState(new TranslateState());
 		}
 
 		#if mobileC
@@ -59,14 +59,14 @@ class FlashingState extends MusicBeatState
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
 						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new TitleStateFirst());
+							MusicBeatState.switchState(new TitleState());
 						});
 					});
 				} else {
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
 						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleStateFirst());
+							MusicBeatState.switchState(new TitleState());
 						}
 					});
 				}
