e04c2650b225567aa06e402ced6b7f8225e1630b
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-08-01 06:10:38 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -88,7 +88,7 @@ class PlayState extends MusicBeatState
 		['Bruh', 0.6], //From 50% to 59%
 		['Meh', 0.69], //From 60% to 68%
 		['Joli', 0.7], //69%
-		['Bon' 0.8], //From 70% to 79%
+		['Bon', 0.8], //From 70% to 79%
 		['Génial', 0.9], //From 80% to 89%
 		['Malade!', 1], //From 90% to 99%
 		['Parfait!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
