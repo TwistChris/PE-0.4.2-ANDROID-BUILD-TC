fc3a81b779de7bb871199e8b979948b6650e5182
TwistChris <95858593+TwistChris@users.noreply.github.com>
2022-08-02 12:28:46 +0200

    Update LanguageState.hx

--- source/options/LanguageState.hx
+++ source/options/LanguageState.hx
@@ -87,7 +87,7 @@ class LanguageState extends MusicBeatState
                        FlxTransitionableState.skipNextTransOut = true;
                        ClientPrefs.translate = false;
 
-                       languagescore = lang[curSelected];
+                       PlayState.languagescore = lang[curSelected];
                        ClientPrefs.language = lang[curSelected];
 		       Reflect.setProperty(ClientPrefs, 'language', lang[curSelected]);
 		       ClientPrefs.saveSettings();
